a1bd7ffc009f15a9987d17004354280ca929291c
Damien Laureaux <d.laureaux@timoa.com>
2022-05-22 01:09:12 +0200

    fix(cloudinit): update the path to the template + cleanup (WIP)

--- terraform/cloudinit.tf
+++ terraform/cloudinit.tf
@@ -1,5 +1,5 @@
 locals {
-  cloudinit = templatefile("${path.root}/cloudinit/template.tftpl", {
+  cloudinit = templatefile("${path.root}/templates/cloudinit.tftpl", {
     user           = var.instance_os_user
     vscode_version = var.vscode_version
   })
@@ -17,40 +17,26 @@ data "cloudinit_config" "cloudinit" {
 }
 
 # Mount the Block Volume + enable VSCode Server
-resource "null_resource" "post_install" {
-  depends_on = [
-    oci_core_instance.instance,
-    oci_core_volume_attachment.volume_attachment,
-  ]
+# resource "null_resource" "post_install" {
+#   depends_on = [
+#     oci_core_instance.instance,
+#     oci_core_volume_attachment.volume_attachment,
+#   ]
 
-  triggers = {
-    volume_attachment_id = oci_core_volume_attachment.volume_attachment.id # Trigger on volume attachment changes
-  }
+#   triggers = {
+#     volume_attachment_id = oci_core_volume_attachment.volume_attachment.id # Trigger on volume attachment changes
+#   }
 
-  provisioner "remote-exec" {
-    connection {
-      agent       = false
-      timeout     = "30m"
-      host        = oci_core_instance.instance.public_ip
-      user        = var.instance_os_user
-      private_key = tls_private_key.default[0].private_key_openssh
-    }
+#   provisioner "remote-exec" {
+#     connection {
+#       agent       = false
+#       timeout     = "30m"
+#       host        = oci_core_instance.instance.public_ip
+#       user        = var.instance_os_user
+#       private_key = tls_private_key.default[0].private_key_openssh
+#     }
 
-    inline = [
-      "sudo mkdir -p /data",
-      "sudo chown ${var.instance_os_user}:${var.instance_os_user} /data", # Add user permission to /data
-      "sudo iscsiadm -m node -o new -T ${oci_core_volume_attachment.volume_attachment.iqn} -p ${oci_core_volume_attachment.volume_attachment.ipv4}:${oci_core_volume_attachment.volume_attachment.port}",
-      "sudo iscsiadm -m node -o update -T ${oci_core_volume_attachment.volume_attachment.iqn} -n node.startup -v automatic",
-      "sudo iscsiadm -m node -T ${oci_core_volume_attachment.volume_attachment.iqn} -p ${oci_core_volume_attachment.volume_attachment.ipv4}:${oci_core_volume_attachment.volume_attachment.port} -o update -n node.session.auth.authmethod -v CHAP",
-      "sudo iscsiadm -m node -T ${oci_core_volume_attachment.volume_attachment.iqn} -p ${oci_core_volume_attachment.volume_attachment.ipv4}:${oci_core_volume_attachment.volume_attachment.port} -o update -n node.session.auth.username -v ${oci_core_volume_attachment.volume_attachment.chap_username}",
-      "sudo iscsiadm -m node -T ${oci_core_volume_attachment.volume_attachment.iqn} -p ${oci_core_volume_attachment.volume_attachment.ipv4}:${oci_core_volume_attachment.volume_attachment.port} -o update -n node.session.auth.password -v ${oci_core_volume_attachment.volume_attachment.chap_secret}",
-      "sudo iscsiadm -m node -T ${oci_core_volume_attachment.volume_attachment.iqn} -p ${oci_core_volume_attachment.volume_attachment.ipv4}:${oci_core_volume_attachment.volume_attachment.port} -l",
-      "sleep 5",                                                                                                                            # Wait for the volume to be mounted
-      "sudo blkid $(readlink -f ${var.block_volume_device_name}) || sudo mkfs -t ext4 $(readlink -f ${var.block_volume_device_name})",      # Format the volume if needed
-      "sudo e2label $(readlink -f ${var.block_volume_device_name}) instance-data",                                                          # Set the label
-      "sudo sed  -i -e '/^[\\/][^ \t]*[ \t]*\\/data[ \t]/d' /etc/fstab",                                                                    # Remove the old entry
-      "sudo grep -q ^LABEL=instance-data /etc/fstab || echo 'LABEL=instance-data /data ext4 defaults' | sudo tee -a /etc/fstab >/dev/null", # Add the new entry
-      "sudo grep -q \"^$(readlink -f ${var.block_volume_device_name}) /data \" /proc/mounts || sudo mount /data",                           # Mount the volume to /data                                                                                    # Enable the Code Server at boot
-    ]
-  }
-}
+#     inline = [                                                                              # Enable the Code Server at boot
+#     ]
+#   }
+# }
