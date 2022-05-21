1818695df13bb2fada564ed39f4736826960abe7
Damien Laureaux <d.laureaux@timoa.com>
2022-05-21 13:52:09 +0200

    fix(project): fix previous messy commit

--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.private_key
-  region       = var.region
-}
