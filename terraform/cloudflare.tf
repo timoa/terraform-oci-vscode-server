367af1d585f3169e153ff3693f802f69060339b3
Damien Laureaux <d.laureaux@timoa.com>
2022-05-29 01:30:07 +0200

    feat(cloudflare): create the Cloudflare Access Tunnel

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,5 +1,8 @@
 locals {
-  cf_zone_id = data.cloudflare_zone.cf_zone.id
+  cf_zone_id     = data.cloudflare_zone.cf_zone.id
+  cf_cname       = "${var.cf_subdomain}.${var.cf_domain}"
+  cf_tunnel_name = "${var.namespace}-tunnel-${var.stage}"
+  cf_argo_secret = random_id.argo_secret.b64_std
 }
 
 # Get the zone ID for the given domain
@@ -7,10 +10,22 @@ data "cloudflare_zone" "cf_zone" {
   name = var.cf_domain
 }
 
+# Create a secret for the Cloudflare Argo Tunnel
+resource "random_id" "argo_secret" {
+  byte_length = 35
+}
+
+# Create the Cloudflare Argo Tunnel for Cloudflare Access
+resource "cloudflare_argo_tunnel" "cf_tunnel" {
+  account_id = var.cf_account_id
+  name       = local.cf_tunnel_name
+  secret     = local.cf_argo_secret
+}
+
 # Create the Cloudflare Access Application
 resource "cloudflare_access_application" "cf_application" {
   zone_id          = local.cf_zone_id
-  domain           = var.cf_domain
+  domain           = local.cf_cname
   name             = "VSCode Server"
   type             = "self_hosted"
   logo_url         = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png"
