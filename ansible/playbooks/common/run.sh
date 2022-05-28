876176c62f61a154caf5205a8bbe2deb7066988c
Damien Laureaux <d.laureaux@timoa.com>
2022-05-29 01:26:29 +0200

    fix(ansible): reboot only if there is kernel updates that really need a reboot

--- ansible/playbooks/common/run.sh
+++ ansible/playbooks/common/run.sh
@@ -8,14 +8,8 @@ BASEDIR=$(dirname $0)
 # Install updates
 ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/os-updates.yml
 
-# Reboot the server
-ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/reboot.yml
-
 # Install VS Code Server
 ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/vscode-server.yml
 
 # Install the other required packages
 ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/playbook.yml
-
-# Install the Cloudflare Agent
-ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/cloudflare-agent.yml
