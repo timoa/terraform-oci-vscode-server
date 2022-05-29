d495236b00ae1de9b178b2589b1d91303969ff27
Damien Laureaux <d.laureaux@timoa.com>
2022-05-29 17:55:06 +0200

    feat(cloudflare): create the Cloudflare Tunnel automatically and allow only list of users

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -10,6 +10,10 @@ data "cloudflare_zone" "cf_zone" {
   name = var.cf_domain
 }
 
+#############################
+# Cloudflare Access Tunnel
+#############################
+
 # Create a secret for the Cloudflare Argo Tunnel
 resource "random_id" "argo_secret" {
   byte_length = 35
@@ -22,6 +26,27 @@ resource "cloudflare_argo_tunnel" "cf_tunnel" {
   secret     = local.cf_argo_secret
 }
 
+# Create the Cloudfflare Argo Tunnel Route
+resource "cloudflare_tunnel_route" "cf_tunnel_route" {
+  account_id = var.cf_account_id
+  tunnel_id  = cloudflare_argo_tunnel.cf_tunnel.id
+  network    = "${oci_core_instance.instance.private_ip}/32"
+  comment    = "Tunnel Route for VSCode Server"
+}
+
+# Create the Cloudflare DNS Record for the Cloudflare Argo Tunnel
+resource "cloudflare_record" "cf_tunnel_cname" {
+  zone_id = local.cf_zone_id
+  name    = var.cf_subdomain
+  value   = "${cloudflare_argo_tunnel.cf_tunnel.id}.cfargotunnel.com"
+  type    = "CNAME"
+  proxied = true
+}
+
+#############################
+# Cloudflare Access App
+#############################
+
 # Create the Cloudflare Access Application
 resource "cloudflare_access_application" "cf_application" {
   zone_id          = local.cf_zone_id
@@ -30,4 +55,20 @@ resource "cloudflare_access_application" "cf_application" {
   type             = "self_hosted"
   logo_url         = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png"
   session_duration = "24h"
+}
+
+# Create the Cloudflare Access Application Allow Policy
+resource "cloudflare_access_policy" "cf_allow_policy" {
+
+  count = length(var.cf_allowed_users) != 0 ? 1 : 0
+
+  application_id = cloudflare_access_application.cf_application.id
+  zone_id        = local.cf_zone_id
+  name           = "Allow Policy"
+  precedence     = "1"
+  decision       = "allow"
+
+  include {
+    email = var.cf_allowed_users
+  }
 }
