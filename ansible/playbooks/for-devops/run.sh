a10076a8e3f10c67ac103a1a2c196c8b5f5690d0
Damien Laureaux <d.laureaux@timoa.com>
2022-06-06 08:02:45 +0200

    fix(ansible): fix HELM install + make DevOps playbook optional

--- ansible/playbooks/for-devops/run.sh
+++ ansible/playbooks/for-devops/run.sh
@@ -2,8 +2,11 @@
 
 BASEDIR=$(dirname $0)
 
-# Install the Ansible roles
-ansible-galaxy install -r $BASEDIR/roles.yml
+if [ "$1" = "enabled" ]
+then
+    # Install the Ansible roles
+    # ansible-galaxy install -r $BASEDIR/roles.yml
 
-# Run the playbook
-ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/playbook.yml
+    # Run the playbook
+    ansible-playbook -i $BASEDIR/../../hosts.yml $BASEDIR/playbook.yml
+fi
