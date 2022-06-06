34cd09779e77284c8926741f15960c117df4e258
Damien Laureaux <d.laureaux@timoa.com>
2022-06-06 07:56:50 +0200

    fix(cloudinit): pre-install Ansible with cloud init

--- terraform/cloudinit.tf
+++ terraform/cloudinit.tf
@@ -1,14 +1,13 @@
 locals {
   cloudinit = templatefile("${path.root}/templates/cloudinit.tftpl", {
-    user           = var.instance_os_user
-    vscode_version = var.vscode_version
+    user = var.instance_os_user
   })
 }
 
 # Generate the Cloud Init config file
 data "cloudinit_config" "cloudinit" {
 
-  # Configure the instance + install VSCode Server
+  # Configure the instance
   part {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
